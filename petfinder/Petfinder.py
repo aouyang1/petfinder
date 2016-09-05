a33f73675596948e6708eb9202b15b4ea42601fb
aouyang1 <aouyang1@gmail.com>
2016-09-04 17:07:51 -0700

    added cleaning function for  tag and refined outputs to be consistent for lists and dicts

--- petfinder/Petfinder.py
+++ petfinder/Petfinder.py
@@ -5,43 +5,48 @@ class Petfinder(object):
         self.key = key
         self.endpoint = "http://api.petfinder.com/"
 
-    def build_request(self, method, args):
-        if type(args) is not dict:
-            return
-
-        arg_text = ""
-        for item in args:
-            print(item)
-            arg_text += "".join([str(item),"=",str(args[item]),"&"])
-
-        return self.endpoint + method + "?" + arg_text
-
     def remove_extra_tag(self, d):
-        if not isinstance(d,dict):
+        if not isinstance(d, dict):
+            if isinstance(d, list):
+                for item in d:
+                    self.remove_extra_tag(item)
             return
 
         for k,v in d.items():
-            if isinstance(d[k],dict):
-                self.remove_extra_tag(d[k])
-        
+            # check if current key holds a dict containing $t as a key
+            if isinstance(d[k], dict) and "$t" in d[k]:
+                d[k] = d[k]["$t"]
+
+            # check if list is composed of dicts with $t as a key
+            if isinstance(d[k], list) and len(d[k])>0 and "$t" in d[k][0]:
+                d[k] = list(map(lambda x: x["$t"], d[k]))
+
+            # continue searching into json payload
+            self.remove_extra_tag(d[k])
 
     def breed_list(self, animal):
         args = {"key": self.key,
                 "animal": animal,
                 "format": "json"}
 
-        req = self.build_request("breed.list", args)
+        api_endpoint = "".join([self.endpoint, "breed.list"])
 
-        return requests.get(req).json()
+        resp = requests.get(api_endpoint, params=args).json()
+        self.remove_extra_tag(resp)
+
+        return resp['petfinder']['breeds']['breed']
 
     def pet_get(self, id):
         args = {"key": self.key,
                 "id": id,
                 "format": "json"}
 
-        req = self.build_request("pet.get", args)
+        api_endpoint = "".join([self.endpoint, "pet.get"])
 
-        return requests.get(req).json()
+        resp = requests.get(api_endpoint, params=args).json()
+        self.remove_extra_tag(resp)
+
+        return resp['petfinder']['pet']
 
     def shelter_find(self, location, offset=0, count=25):
         args = {"key": self.key,
@@ -50,9 +55,14 @@ class Petfinder(object):
                 "count": count,
                 "format": "json"}
 
-        req = self.build_request("shelter.find", args)
+        api_endpoint = "".join([self.endpoint, "shelter.find"])
 
-        return requests.get(req).json()
+        resp = requests.get(api_endpoint, params=args).json()
+        self.remove_extra_tag(resp)
+
+        output = resp['petfinder']['shelters']['shelter']
+
+        return output if isinstance(output, list) else [output]
 
     def shelter_get_pets(self, id, status="A", offset=0, count=25, output="basic"):
         args = {"key": self.key,
@@ -63,9 +73,14 @@ class Petfinder(object):
                 "output": output,
                 "format": "json"}
 
-        req = self.build_request("shelter.getPets", args)
+        api_endpoint = "".join([self.endpoint, "shelter.getPets"])
 
-        return requests.get(req).json()
+        resp = requests.get(api_endpoint, params=args).json()
+        self.remove_extra_tag(resp)
+
+        output = resp['petfinder']['pets']['pet']
+
+        return output if isinstance(output, list) else [output]
 
 
 
--- petfinder/test.py
+++ petfinder/test.py
@@ -6,4 +6,14 @@ PF_KEY=os.environ["PF_KEY"]
 
 p = Petfinder(PF_KEY)
 
-pprint(p.shelter_find("94403"))
+resp = p.shelter_find(location="94403", count=1)
+pprint(resp)
+
+blist = p.breed_list(animal="horse")
+pprint(blist)
+
+shelter = p.shelter_get_pets(id="CA690", count=1)
+pprint(shelter)
+
+pet = p.pet_get(id="11914725")
+pprint(pet)
