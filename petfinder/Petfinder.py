ceaa8e0dc3544cc160e0214e86b47ead5bbf0a5c
aouyang1 <aouyang1@gmail.com>
2016-09-04 20:47:55 -0700

    rough scraper for all shelters in us

--- petfinder/Petfinder.py
+++ petfinder/Petfinder.py
@@ -36,6 +36,39 @@ class Petfinder(object):
 
         return resp['petfinder']['breeds']['breed']
 
+    def pet_find(self, location, animal=None, breed=None, size=None, sex=None, age=None, count=25, offset=0):
+        args = {"key": self.key,
+                "location": location,
+                "count": count,
+                "offset": offset,
+                "format": "json"}
+
+        if offset % count != 0:
+            print("invalid offset. must be multiple of count")
+            return []
+
+        if animal is not None:
+            args["animal"] = animal
+
+        if breed is not None:
+            args["breed"] = breed
+
+        if size is not None:
+            args["size"] = size
+
+        if sex is not None:
+            args["sex"] = sex
+
+        if age is not None:
+            args["age"] = age
+
+        api_endpoint = "".join([self.endpoint, "pet.find"])
+
+        resp = requests.get(api_endpoint, params=args).json()
+        self.remove_extra_tag(resp)
+
+        return resp['petfinder']['pets']['pet']
+
     def pet_get(self, id):
         args = {"key": self.key,
                 "id": id,
@@ -48,27 +81,31 @@ class Petfinder(object):
 
         return resp['petfinder']['pet']
 
-    def shelter_find(self, location, offset=0, count=25):
+    def shelter_find(self, location, count=25, offset=0):
         args = {"key": self.key,
                 "location": location,
-                "offset": offset,
                 "count": count,
+                "offset": offset,
                 "format": "json"}
+
+        if offset % count != 0:
+            print("invalid offset. must be multiple of count")
+            return []
 
         api_endpoint = "".join([self.endpoint, "shelter.find"])
 
         resp = requests.get(api_endpoint, params=args).json()
         self.remove_extra_tag(resp)
 
-        output = resp['petfinder']['shelters']['shelter']
+        shelters = resp['petfinder']['shelters']
+        output = shelters['shelter'] if 'shelter' in shelters else []
 
         return output if isinstance(output, list) else [output]
 
-    def shelter_get_pets(self, id, status="A", offset=0, count=25, output="basic"):
+    def shelter_get_pets(self, id, status="A", count=25, output="basic"):
         args = {"key": self.key,
                 "id": id,
                 "status": status,
-                "offset": offset,
                 "count": count,
                 "output": output,
                 "format": "json"}
@@ -78,7 +115,8 @@ class Petfinder(object):
         resp = requests.get(api_endpoint, params=args).json()
         self.remove_extra_tag(resp)
 
-        output = resp['petfinder']['pets']['pet']
+        pets = resp['petfinder']['pets']
+        output = pets['pet'] if 'pet' in pets else []
 
         return output if isinstance(output, list) else [output]
 
--- petfinder/test.py
+++ petfinder/test.py
@@ -1,19 +1,43 @@
 from Petfinder import Petfinder
 import os
 from pprint import pprint
+import json
+import csv
+from time import sleep
 
 PF_KEY=os.environ["PF_KEY"]
 
 p = Petfinder(PF_KEY)
 
-resp = p.shelter_find(location="94403", count=1)
-pprint(resp)
+f_zip = open('us_postal_codes.csv','r')
+f_data = csv.reader(f_zip)
 
-blist = p.breed_list(animal="horse")
-pprint(blist)
+zip_codes = []
+for record in f_data:
+    zip_code = record[0]
+    if len(zip_code) > 0:
+        try:
+            zip_int = int(zip_code)
+            zip_codes.append(zip_code)
+        except:
+            print("must be a number as string")
 
-shelter = p.shelter_get_pets(id="CA690", count=1)
-pprint(shelter)
+print(len(zip_codes))
 
-pet = p.pet_get(id="11914725")
-pprint(pet)
+f = open('shelter.json', 'w')
+
+
+page_size = 1000
+
+shelter_dict = {}
+
+for zipcode in zip_codes:
+    sleep(2)
+    shelters = p.shelter_find(location=zipcode,count=page_size)
+    for shelter in shelters:
+        if shelter["id"] not in shelter_dict:
+            shelter_dict[shelter["id"]] = 1
+
+            print(shelter["state"], shelter["city"], shelter["id"], len(shelter_dict))
+            f.write("{}\n".format(json.dumps(shelter)))
+
