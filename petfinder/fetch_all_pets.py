3ab2fb0cc4b4913fda32ed19394649fee4bdae3e
Austin Ouyang <aouyang1@gmail.com>
2016-09-05 21:21:09 -0700

    catch exceptions while collecting pets and updated postal code list

--- petfinder/fetch_all_pets.py
+++ petfinder/fetch_all_pets.py
@@ -8,10 +8,17 @@ from time import sleep
 PF_KEY=os.environ["PF_KEY"]
 p = Petfinder(PF_KEY)
 
-f_pets = open('pets.json', 'w')
+part="al"
 
-with open('shelter.json', 'r') as f:
+f_pets = open('pets-{}.json'.format(part), 'w')
+
+with open('part-{}'.format(part), 'r') as f:
     for line in f:
-        pets = p.shelter_get_pets(json.loads(line)['id'], count=1000)
-        for pet in pets:
-            f_pets.write("{}\n".format(json.dumps(pet)))
+        shelter_id = json.loads(line)['id']
+        try:
+            print("fetching pets from shelder id: {}".format(shelter_id))
+            pets = p.shelter_get_pets(shelter_id, count=1000)
+            for pet in pets:
+                f_pets.write("{}\n".format(json.dumps(pet)))
+        except:
+            print("could not fetch pets from shelter id: {}".format(shelter_id))
